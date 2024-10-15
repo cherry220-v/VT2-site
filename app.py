e4c4eaf7b938103fd7802ebaff4477b914af5661
cherry220-v <crispycrabber@gmail.com>
2024-10-16 02:00:32 +0300

    first commit

---
+++ app.py
@@ -0,0 +1,22 @@
+from flask import Flask, render_template, send_file
+import os, zipfile
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/update', methods=["GET"])
+def updatePlugins():
+    if os.path.isfile("plugins.zip"): os.remove("plugins.zip")
+    zipf = zipfile.ZipFile("plugins.zip", "w")
+    for f in os.listdir("plugins"):
+        zipf.write(os.path.join("plugins", f))
+    zipf.close()
+    return send_file("plugins.zip")
+
+if __name__ == '__main__':
+    app.run(host='127.0.0.1', port=8000, debug=True)
+ 
